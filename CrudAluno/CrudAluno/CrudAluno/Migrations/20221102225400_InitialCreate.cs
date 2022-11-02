e012aa9680f13f3eedcbf5efd1df122986b4f060
Andrew Oliveira Benevides <andrew.benevides@w3k.com.br>
2022-11-02 19:56:30 -0300

    adicionado pacotes de nugts novos e junto as migration do banco

---
+++ CrudAluno/CrudAluno/CrudAluno/Migrations/20221102225400_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudAluno.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    RA = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CPF = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.RA);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alunos");
+        }
+    }
+}
--- CrudAluno/CrudAluno/CrudAluno/Model/Aluno.cs
+++ CrudAluno/CrudAluno/CrudAluno/Model/Aluno.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace CrudAluno.Model
 {
     public class Aluno
     {
+        [Key]
+        public int RA { get; set; }
+        public string CPF { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
